548c85f279dde911efd650c0249596f100683fcb
Sophia Cho <57773045+scho324@users.noreply.github.com>
2026-09-14 00:15:03 -0400

    Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/todo-tasks/",
   plugins: [react(), tailwindcss()],
-  base: "/OOSE-HW1/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
